f811b62f559ba8275dbdb962448e6cd32e3f9f0f
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-08-19 23:48:43 -0500

    3/4 done with code

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 //When page loads a timer starts to countdown
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -16,27 +17,142 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#timerDisplay');
-    startTimer(fiveMinutes, display);
-};
+let question = document.querySelector('#question');
+let choices = document.querySelectorAll('.choice-text');
 
-function generateQuiz(questions, quizContainer, results, submitButton) {
+let currentQuestion = {};
+let acceptingAnswers = true;
+let questionCounter = 0;
+let availableQuestions = [];
 
-    function showQuestions(questions, quizContainer) {
-        // code will go here
-    }
+let questions = [
+    {
+        question: 'What is ECMA6?',
+        choice1: 'Version of JavaScript',
+        choice2: 'Version of HTML',
+        choice3: 'Version of CSS',
+        choice4: 'Version of BootStrap',
+        answer: 1,
+    },
+    {
+        question: 'Arrays can be used to store?',
+        choice1: 'Variables',
+        choice2: 'Numbers',
+        choice3: 'Strings',
+        choice4: 'Booleans',
+        answer: 3,
+    },
+    {
+        question: 'What is a function?',
+        choice1: ' set of statements that performs a task',
+        choice2: 'different types of variables',
+        choice3: 'a set of arrays in a variable',
+        choice4: 'undefined',
+        answer: 1,
+    },
+    {
+        question: 'What is a boolean?',
+        choice1: 'True or False',
+        choice2: 'Numbers',
+        choice3: 'A dataset',
+        choice4: 'a type of method',
+        answer: 1,
+    },
+    {
+        question: 'What is pseudo coding?',
+        choice1: 'a cool way to code',
+        choice2: 'writing codes in comment',
+        choice3: 'hacking using coding',
+        choice4: 'all the above',
+        answer: 2,
+    },
+    {
+        question: 'What case is JavaScript?',
+        choice1: 'snake case',
+        choice2: 'lower case',
+        choice3: 'pipe case',
+        choice4: 'camel case',
+        answer: 4,
+    },
+    {
+        question: 'What type of language is JavaScript?',
+        choice1: 'static',
+        choice2: 'dynamic',
+        choice3: 'parallel',
+        choice4: 'role-oriented',
+        answer: 2,
+    },
+    {
+        question: 'What is CSS used for?',
+        choice1: 'to style HTML content',
+        choice2: 'to style JavaScript content',
+        choice3: 'to use math',
+        choice4: 'to logically use',
+        answer: 2,
+    },
+    {
+        question: 'When was HTML created?',
+        choice1: '1999',
+        choice2: '1975',
+        choice3: '1950',
+        choice4: '1989',
+        answer: 4,
+    },
+    {
+        question: 'What is JSON used for',
+        choice1: 'representing structured dat based on JS object syntax',
+        choice2: 'to help push files in GitHub',
+        choice3: 'a library for JavaScript',
+        choice4: 'to structure code easier',
+        answer: 2,
+    },
+]
 
-    function showResults(questions, quizContainer, results) {
-        // code will go here
-    }
+const maxQuestions = 10;
 
-    // show the questions
-    showQuestions(questions, quizContainer);
-
-    // when user clicks submit, show results
-    submitButton.onclick = function () {
-        showResults(questions, quizContainer, results);
-    }
+function startGame() {
+    questionCounter = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
 }
+
+function getNewQuestion() {
+    questionCounter++;
+
+    let questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        let number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionsIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+choices.forEach(choices => {
+    choices.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        let selectedChoice = e.target;
+        let selectedAnswer = selectedChoice.dataset['number'];
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? '.correct' : '.incorrect';
+
+        if (classToApply === '.correct') {
+        }
+
+        selectedChoice.parentElement.classList.addEventListener('click', classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+startGame()
